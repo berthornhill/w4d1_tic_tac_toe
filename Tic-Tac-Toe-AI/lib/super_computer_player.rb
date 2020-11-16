ca273afa2b8eaf3504a028c44f10fda716fd2475
Zihao Li <pockyhao518@gmail.com>
2020-11-16 17:27:40 -0500

    cpu is building

--- Tic-Tac-Toe-AI/lib/super_computer_player.rb
+++ Tic-Tac-Toe-AI/lib/super_computer_player.rb
@@ -2,6 +2,14 @@ require_relative 'tic_tac_toe_node'
 
 class SuperComputerPlayer < ComputerPlayer
   def move(game, mark)
+    cpu = TicTacToeNode.new(game.board, mark)
+    if cpu.winning_node?(mark)
+      possible_move = cpu.children
+      possible_move.each do |node|
+      end
+    elsif cpu.losing_node?(mark)
+
+    end
   end
 end
 
